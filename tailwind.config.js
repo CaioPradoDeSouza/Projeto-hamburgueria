6eb4da35a12ec896f8e71898c094a8325c950682
Caio Prado de Souza <cpssouza7@gmail.com>
2024-03-26 21:48:32 -0300

    update project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    fontFamily:{
+      'sans':
+      ['Roboto' , 'sans-serif']
+    },
+
     extend: {
       backgroundImage:{
         "home":"url('/assets/bg.png')"
